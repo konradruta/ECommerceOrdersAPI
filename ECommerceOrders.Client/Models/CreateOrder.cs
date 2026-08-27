639f0f3e507127cf3781fcb09b34dbef4b73c2dd
Konrad Ruta <konrad.rutaa@gmail.com>
2026-08-27 18:17:42 +0200

    Init change

---
+++ ECommerceOrders.Client/Models/CreateOrder.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerceOrders.Client.Models
+{
+    public class CreateOrder
+    {
+        public List<CreateOrderItem> Products { get; set; } = [];
+    }
+}
---
+++ ECommerceOrders.Client/Models/OrderProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceOrders.Client.Models
+{
+    public class OrderProduct
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = "";
+        public int Quantity { get; set; }
+    }
+}
